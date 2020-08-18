83154c1a2438826cd14b4cfa3716bec038e5bf31
Patrick Brown <pjbrown05@gmail.com>
2020-08-18 14:28:10 -0500

    added playercard

--- src/pages/Roster.js
+++ src/pages/Roster.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../../src/App.css';
+import PlayerCard from '../components/PlayerCard/PlayerCard';
 
 function Roster() {
   return (
     <div>
       <h1>Roster</h1>
+      <PlayerCard />
     </div>
   );
 }
